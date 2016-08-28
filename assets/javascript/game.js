a2a94a07935a3cc84ac6e22f055ddf5a9a6e71fa
aksm <albert.min@gmail.com>
2016-08-28 17:21:52 -0400

    Update round one fighting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,6 +13,7 @@ var game = {
 	// Gameplay variables
 	player: "",
 	playerhp: 0,
+	playerap: 0,
 	opponent: "",
 	opponenthp: 0,
 	characters: [],
@@ -30,12 +31,13 @@ var game = {
 		});
 	},
 	picked: function(e, p) {
-		e.html("<div class = 'character-container' data-name ="+p+">" + game.players[p].name + "<img src='"+game.players[p].pic+"' class='character'>"+game.players[p].hp+"</div>");
+		e.html("<div class = 'character-container' data-name ="+p+">" + game.players[p].name + "<img src='"+game.players[p].pic+"' class='character'><p>"+game.players[p].hp+"</p></div>");
 	},
 	updatePlayers: function(c) {
 		if(this.player == "") {
 	 		this.player = c;
 	 		this.playerhp = this.players[this.player].hp;
+	 		this.playerap = this.players[this.player].ap;
 	 		this.picked(this.playerElement, this.player);
 	 		this.characters.splice(this.characters.indexOf(this.player),1);
 	 		this.choosePlayer(this.opponentsElement);
@@ -49,10 +51,13 @@ var game = {
 
 	},
 	fight: function() {
-		if(this.player != "" && this.opponent != "") {
-			console.log("Fight!");
-			
-		}
+		if(this.player != "" && this.opponent != "" && this.opponenthp > 0 && this.playerhp > 0) {
+			this.opponenthp = Math.max(0, this.opponenthp - this.playerap);
+			$("[data-name='"+this.opponent+"']").children("p").html(this.opponenthp);
+			this.playerhp = Math.max(0, this.playerhp - game.players[this.opponent].cap);
+			$("[data-name='"+this.player+"']").children("p").html(this.playerhp);
+			this.playerap = this.playerap + game.players[this.player].ap;
+		} //else if ()
 	}
 }
 
@@ -66,7 +71,11 @@ $(document).ready(function(){
 	$('body').on("click", ".character-button",function() {
 		var c = $(this).data("name");
 		game.updatePlayers(c);
-		console.log("Player: "+game.playerhp);
-		console.log("Opponent: "+game.opponenthp);
+		console.log("Player HP: "+game.playerhp);
+		console.log("Player AP: "+game.playerap);
+		console.log("Player CAP: "+game.players[game.player].cap);
+		console.log("Opponent HP: "+game.opponenthp);
+		console.log("Opponent AP: "+game.players[game.opponent].ap);
+		console.log("Opponent CAP: "+game.players[game.opponent].cap);
 	});
 });
